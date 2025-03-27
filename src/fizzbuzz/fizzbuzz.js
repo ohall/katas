ffc73c9e8b6b54640a3be7a75a33698c8149f252
ohall <ohall@grassroots-tech.com>
2025-03-26 21:16:43 -0400

    Implement fizzbuzz function to return "Fizz", "Buzz", or "FizzBuzz" based on divisibility by 3 and 5

--- src/fizzbuzz/fizzbuzz.js
+++ src/fizzbuzz/fizzbuzz.js
@@ -9,7 +9,18 @@
  */
 
 function fizzbuzz(number) {
-  // Your implementation here
+  if (number % 3 === 0 &&  number % 5 === 0 ) {
+    return "FizzBuzz";
+  }
+  if (number % 3 === 0) {
+    return "Fizz";
+  }
+
+  if (number % 5 === 0) {
+    return "Buzz"
+  }
+
+  return number
 }
 
 module.exports = fizzbuzz;
